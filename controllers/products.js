ea62a85b0245b877202e7385607b4204ee3fbd65
Toto Argumedo <totoargumedo@gmail.com>
2022-11-16 19:34:32 -0300

    desafio 5 con handlebars listo

--- controllers/products.js
+++ controllers/products.js
@@ -4,22 +4,31 @@ const productos = new ContainerFile("productos");
 
 async function getProduct({ params }, res) {
   let findElement;
+  let multipleElements;
   if (params.id) {
     findElement = await productos.getById(params.id);
+    multipleElements = false;
   } else {
     findElement = await productos.getAll();
+    multipleElements = true;
   }
-  res.status(200).json(findElement);
+  res
+    .status(200)
+    .render("products", { elements: findElement, multiple: multipleElements });
 }
 
 async function getProductRandom(req, res) {
   const getRandomElement = await productos.getRandom();
-  res.status(200).json(getRandomElement);
+  res.status(200).render("product", { element: getRandomElement });
 }
 
 async function saveProduct({ body }, res) {
   const savedProduct = await productos.save(body);
-  res.status(201).json({ message: "Producto cargado", id: savedProduct.id });
+  res
+    .status(201)
+    .redirect(
+      `/?added=true&title=${savedProduct.title}&image=${savedProduct.thumbnail}`
+    );
 }
 
 async function deleteProduct({ params }, res) {
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { routerProductos } from "./routers/products.js";
+import { engine } from "express-handlebars";
 
 // declaracion de server
 const app = express();
@@ -10,15 +11,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // static
-app.use(express.static("public"));
+app.use(express.static("public", { index: false }));
+
+// views engine
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
 
 // Routers
 app.use("/api/productos", routerProductos);
 
-// Endpoints
-// app.get("/", (req, res) => {
-//   res.send("HOLO");
-// });
+// Endpoints;
+app.get("/", (req, res) => {
+  console.log(req.query.added);
+  res.render("productForm", {
+    productTitle: req.query.title,
+    productImage: req.query.image,
+    added: req.query.added,
+  });
+});
 
 // Inicializacion de server
 const server = app.listen(PORT, () => {
